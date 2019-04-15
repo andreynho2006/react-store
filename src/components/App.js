b8128e3d966523bc8c7e2f8a038fc6eb9c19f72b
andreynho2006 <andreynho2006@gmail.com>
2019-04-15 15:09:22 +0300

    import sampleFishes and make a new method to populate the state when an event calls it

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
     state = {
@@ -15,9 +16,16 @@ class App extends React.Component {
         fishes[`fish${Date.now()}`] = fish;
         //set the new fishes object to state
         this.setState({
-            fishes: fishes
+            fishes: fishes  //(fishes)
         });
+    };
+
+    loadSampleFishes = () => {
+        this.setState({
+            fishes: sampleFishes
+        }); 
     }
+
 
     render() {       
         return (
@@ -26,7 +34,7 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market"/>
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish} />    
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>    
             </div>
         );
     }
