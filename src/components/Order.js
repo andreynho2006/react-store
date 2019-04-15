69c361393baeb2080c3d8458cd19384ed5a395ec
andreynho2006 <andreynho2006@gmail.com>
2019-04-15 17:21:44 +0300

    get all the name as keys from order object, get the total price from each orderIds and return the total price

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 class Order extends React.Component {
     render() {
+        const orderIds = Object.keys(this.props.order);
+        const total = orderIds.reduce((prevTotal, key) => {
+            const fish = this.props.fishes[key];
+            const count = this.props.order[key];
+            const isAvailable = fish && fish.status == 'available';
+            if(isAvailable) {
+                return prevTotal + (count * fish.price);
+            }
+            return prevTotal;
+        }, 0);
+
         return (
-            <p>Oder!!!!!</p>
+            <div className="order-wrap">
+                <h2>Order</h2>
+                {orderIds}
+                <div className="total">
+                    <strong>{total}</strong>
+                </div>
+            </div>
         );
     }
 }
