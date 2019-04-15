115c6009961b20470c0fda1abf17c9b84b35c0e1
andreynho2006 <andreynho2006@gmail.com>
2019-04-15 13:59:38 +0300

    add a class to the form and a button to submit

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -3,12 +3,13 @@ import React from 'react';
 class AddFishForm extends React.Component {
     render() {
         return (
-            <form>
+            <form className="fish-edit">
                 <input name="name" type="text" placeholder="Name" />
                 <input name="price" type="text" placeholder="Price" />
                 <input name="status" type="text" placeholder="Status" />
                 <input name="desc" type="text" placeholder="Desc" />
                 <input name="image" type="text" placeholder="Image" />
+                <button type="submit"> + add Fish</button>
             </form>       
         );
     }
