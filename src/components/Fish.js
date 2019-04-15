a8753df42ee706408c9527ce4a4a72533109c120
andreynho2006 <andreynho2006@gmail.com>
2019-04-15 15:50:42 +0300

    make a Fish component , get all details from the props, and display the fish component

---
+++ src/components/Fish.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { formatPrice } from '../helpers';
+
+class Fish extends React.Component {
+    render() {
+        const image = this.props.details.image;
+        const name = this.props.details.name;
+        const price = this.props.details.price;
+        const desc = this.props.details.desc;
+        return (
+            <li className="menu-fish">
+                <img src={image} alt={name} />
+                <h3 className="fish-name">
+                    {name}
+                    <span className="price">{formatPrice(price)}</span>
+                </h3>
+                <p>{desc}</p>
+                <button>Add to cart</button>
+            </li>          
+        );
+    }
+}
+
+export default Fish;
