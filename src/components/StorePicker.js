d1dfbcad9eb2f8a733f90c166a65a4d7a8c833bf
andreynho2006 <andreynho2006@gmail.com>
2019-04-15 18:24:37 +0300

    refactoring the input name of the store

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -9,9 +9,9 @@ class StorePicker extends React.Component {
         event.preventDefault();
         // get the text from that input
         console.log(this.myInput);
-        const storeName = this.myInput.defaultValue;
-        // change the page to store/......
-        this.props.history.push(`/store/${this.storeName}`);
+        const storeName = this.myInput.current.value;
+        // 3. Change the page to /store/whatever-they-entered
+        this.props.history.push(`/store/${storeName}`);
         
     }
     render() {
