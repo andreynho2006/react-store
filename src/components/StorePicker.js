f0cbe93497d359640d97187852bc42714a93b014
andreynho2006 <andreynho2006@gmail.com>
2019-04-15 11:08:15 +0300

    add a form

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>Hello!</p>
+        return(
+            <form action="" className="store-selector">
+                <h2>Plase enter a store</h2>
+                { /* comment */ }
+                <input type="text" required placeholder="Store name" />
+                <button type="submit">Visit store</button>
+            </form>           
+        ) 
     }
 }
 
