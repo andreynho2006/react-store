922af1dc58f76354075ab7ff02ed1fe962a744c0
andreynho2006 <andreynho2006@gmail.com>
2019-04-15 17:38:41 +0300

    make a new method to display the order, check if the fish is available, and display the corresponding messages

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,6 +2,21 @@ import React from 'react';
 import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
+    renderOrder = (key) => {
+        const fish = this.props.fishes[key];
+        const count = this.props.order[key];
+        const isAvailable = fish.status === 'available';
+        if(!isAvailable) {
+            return <li key={key}>
+                Sorry {fish ? fish.name : 'fish'} is no longer available
+            </li>
+        }
+        return <li key={key}>
+            {count} lbs {fish.name}
+
+            {formatPrice(fish.price)}
+        </li>
+    }
     render() {
         const orderIds = Object.keys(this.props.order);
         const total = orderIds.reduce((prevTotal, key) => {
@@ -17,9 +32,11 @@ class Order extends React.Component {
         return (
             <div className="order-wrap">
                 <h2>Order</h2>
-                {orderIds}
+                <ul className="order">
+                    {orderIds.map(this.renderOrder)}
+                </ul>
                 <div className="total">
-                    <strong>{totalPrice(total)}</strong>
+                    <strong>Total:{formatPrice(total)}</strong>
                 </div>
             </div>
         );
