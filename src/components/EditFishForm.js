297c673aa6792691c6ef3398eb55e8dd375b2e56
andreynho2006 <andreynho2006@gmail.com>
2019-04-15 19:45:38 +0300

    make a method to handle the onChange event on the input form and update call updateFish method received from the prop

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -1,17 +1,29 @@
 import React from 'react';
 
 class EditFishForm extends React.Component {
+    handleChange = event => {
+        console.log(event.currentTarget.value);
+        // update that fish
+        // take a copy of the updated fish
+        const updatedFish = { 
+            ...this.props.fish,
+            [event.currentTarget.name]: event.currentTarget.value
+        };
+        //console.log(updatedFish);
+        this.props.updateFish(this.props.index, updatedFish);
+    };
+
     render() {
         return (
             <div className="fish-edit">
-                <input type="text" name="name" value={this.props.fish.name} />
-                <input type="text" name="price" value={this.props.fish.price} />
-                <select type="text" name="status" value={this.props.fish.status} >
+                <input type="text" name="name" onChange={this.handleChange} value={this.props.fish.name} />
+                <input type="text" name="price" onChange={this.handleChange} value={this.props.fish.price} />
+                <select type="text" name="status" onChange={this.handleChange} value={this.props.fish.status} >
                     <option value="available">Fresh!</option>
                     <option value="unavailable">Sold Out!</option>
                 </select>
-                <textarea type="text" name="desc" value={this.props.fish.desc} />
-                <input type="text" name="image" value={this.props.fish.image} />
+                <textarea type="text" name="desc" onChange={this.handleChange} value={this.props.fish.desc} />
+                <input type="text" name="image" onChange={this.handleChange} value={this.props.fish.image} />
             </div>         
         );
     }
