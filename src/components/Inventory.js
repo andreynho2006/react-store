61e38a08c8aabd227b6f56428c8defc2b7d37faa
andreynho2006 <andreynho2006@gmail.com>
2019-04-16 21:15:26 +0300

    add Login component to inventory component to handle login

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AddFishForm from './AddFishForm';
 import EditFishForm from './EditFishForm';
+import Login from './Login';
 import PropTypes from 'prop-types';
 
 class Inventory extends React.Component {
@@ -11,6 +12,7 @@ class Inventory extends React.Component {
         loadSampleFishes: PropTypes.func
     }
     render() {
+        return <Login />
         return (
             <div className="inventory">
                 <h2>Inventory</h2>
