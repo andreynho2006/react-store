9a9063051fecca8618dbc1f8861a887f16c75457
andreynho2006 <andreynho2006@gmail.com>
2019-04-17 17:05:53 +0300

    start using styled-components for Button, Form and Input

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,6 +1,33 @@
 import React from 'react';
+import styled from 'styled-components'
 import { getFunName } from '../helpers';
 import PropTypes from 'prop-types';
+
+const Button = styled.button`
+    text-transform: uppercase;
+    background: none;
+    border: 1px solid #000;
+    font-weight: 600;
+    font-size: 1.5rem;
+    font-family: Open Sans;
+    -webkit-transition: all .2s;
+    transition: all .2s;
+    position: relative;
+    z-index: 2;
+    width: 100%;
+    `;
+
+const Form = styled.form`
+    background: #fff;
+    max-width: 500px;
+    margin: 50px auto;
+    padding: 2rem;
+    border: 2px solid #000;
+    `;
+
+const Input = styled.input`
+    width: 100%;
+    `;
 
 class StorePicker extends React.Component {
     myInput = React.createRef();
@@ -21,18 +48,18 @@ class StorePicker extends React.Component {
     }
     render() {
         return(
-            <form action="" className="store-selector" onSubmit={this.goToStore}>
+            <Form action="" onSubmit={this.goToStore}>
                 <h2>Plase enter a store</h2>
                 { /* comment */ }
-                <input  
+                <Input  
                     type="text" 
                     ref = {this.myInput}
                     required
                     placeholder="Store name" 
                     defaultValue={getFunName()} 
                 />
-                <button type="submit">Visit store</button>
-            </form>           
+                <Button type="submit">Visit store</Button>
+            </Form>           
         ); 
     }
 }
